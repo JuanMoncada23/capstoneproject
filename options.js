c3b133d92c12c7c43b51349c83295964a1df3a2c
JuanMoncada23 <54727154+JuanMoncada23@users.noreply.github.com>
2021-04-09 13:25:54 -0500

    Added layout of pages

--- options.js
+++
@@ -1,7 +0,0 @@
-const options = document.querySelector(".options");
-const sendBtn = document.querySelector(".send");
-
-let OPPONENT; 
-sendBtn.addEventListener("click", function() {
-    if()
-}); 
