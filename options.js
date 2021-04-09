e04afa612fccdc8141f94eaf593f488651813859
Danhong L <lid363@users.noreply.github.com>
2021-04-08 20:57:20 -0500

    April 8 Vision

---
+++ options.js
@@ -0,0 +1,7 @@
+const options = document.querySelector(".options");
+const sendBtn = document.querySelector(".send");
+
+let OPPONENT; 
+sendBtn.addEventListener("click", function() {
+    if()
+}); 
